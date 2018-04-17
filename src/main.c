e06cf4a152de21230cc7cb9c0c343cd2c4bf3981
Lucas David <26874907+lcsdavid@users.noreply.github.com>
2018-04-17 14:57:53 +0200

    main up

--- src/main.c
+++ src/main.c
@@ -1,25 +1,22 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
-#include "standard/node/node.h"
+
 #include <SDL.h>
 
-/*
-int8_t sdl_init(SDL_Window *, SDL_Renderer *);
-void sdl_close(SDL_Window *);
-*/
 
+bool sdl_init(SDL_Window **, SDL_Renderer **);
+void sdl_close(SDL_Window *);
 
 void input();
 void render();
 void update();
 
 int main(int argc, char *argv[]) {
-	/*
 	SDL_Window *sdl_window = NULL;
 	SDL_Renderer *sdl_renderer = NULL;
-	if(sdl_init(&sdl_window, &sdl_renderer) == -1)
+	if(!sdl_init(&sdl_window, &sdl_renderer))
 		return -1;
-	 */
 
 
 	return 0;
@@ -30,40 +27,20 @@ int main(int argc, char *argv[]) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-int8_t sdl_init(SDL_Window *sdl_window, SDL_Renderer *sdl_renderer) {
+bool sdl_init(SDL_Window **sdl_window, SDL_Renderer **sdl_renderer) {
 	if (SDL_Init(SDL_INIT_VIDEO)) {
 		fprintf(stderr, "SDL_Init: %s\n", SDL_GetError());
-		return -1;
+		return false;
 	}
-	SDL_CreateWindowAndRenderer(800, 480, 0, &window, &renderer);
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	return 0;
+	SDL_CreateWindowAndRenderer(800, 480, 0, sdl_window, sdl_renderer);
+	SDL_SetRenderDrawColor(*sdl_renderer, 255, 255, 255, 255);
+	SDL_RenderClear(*sdl_renderer);
+	SDL_RenderPresent(*sdl_renderer);
+	return true;
 }
 
 void sdl_close(SDL_Window *sdl_window) {
-    SDL_DestroyWindow();
+    SDL_DestroyWindow(sdl_window);
     SDL_Quit();
 }
 
@@ -72,8 +49,7 @@ void input() {
     if(!SDL_PollEvent(&event))
         return;
     switch(event.type) {
-
+        default:
+            break;
     }
 }
-
-*/
