f4dbd149ae197339230eb578c3a62b0df7788f46
ThLegras <legrastheo@gmail.com>
2018-04-18 08:24:04 +0200

    correction de change_dir

--- src/gameplay/snake/snake.c
+++ src/gameplay/snake/snake.c
@@ -1,4 +1,7 @@
 #include "snake.h"
+#include "../../standard/math/point.h"
+
+const short int TILE_SIZE = 16;
 
 void snake_grow(snake_t *snake) {
     assert(snake);
@@ -7,9 +10,9 @@ void snake_grow(snake_t *snake) {
 
 void snake_diminish(snake_t *snake);
 
-void change_dir(point_t* last_dir, char new_dir){//note, certaines situations ne sont pas prises en compte, c'est normale hors des cas ci-dessous le serpent doit garder sa trajectoire précèdente
-    if(new_dir == "G" && last_dir->x <= 0 && last_dir->x > -36){//on part du principe que les directions sont modélisées par un cercle de 36 pixels de rayon
-        last_dir->x -= 2;
+void change_dir(point_t *last_dir, char new_dir) {//note, certaines situations ne sont pas prises en compte, c'est normale hors des cas ci-dessous le serpent doit garder sa trajectoire précèdente
+    if(new_dir == 'G' && last_dir->x <= 0 && last_dir->x > -36){//on part du principe que les directions sont modélisées par un cercle de 36 pixels de rayon
+        last_dir->x -= 2;//le serpent doit à chaque tic se déplacer de 36 pixels en valeur absolue
         if(last_dir->y < 0){
             last_dir->y += 2;
         }else{
@@ -17,7 +20,7 @@ void change_dir(point_t* last_dir, char new_dir){//note, certaines situations ne
         }
     }
 
-    if(new_dir == "D" && last_dir->x >= 0 && last_dir->x < 36){//on part du principe que les directions sont modélisées par un cercle de 36 pixels de rayon
+    if(new_dir == 'D' && last_dir->x >= 0 && last_dir->x < 36){
         last_dir->x += 2;
         if(last_dir->y < 0){
             last_dir->y += 2;
@@ -26,7 +29,7 @@ void change_dir(point_t* last_dir, char new_dir){//note, certaines situations ne
         }
     }
 
-    if(new_dir == "H" && last_dir->y <= 0 && last_dir->y > -36){//on part du principe que les directions sont modélisées par un cercle de 36 pixels de rayon
+    if(new_dir == 'H' && last_dir->y <= 0 && last_dir->y > -36){
         last_dir->y -= 2;
         if(last_dir->x < 0){
             last_dir->x += 2;
@@ -35,7 +38,7 @@ void change_dir(point_t* last_dir, char new_dir){//note, certaines situations ne
         }
     }
 
-    if(new_dir == "B" && last_dir->y >= 0 && last_dir->y < 36){//on part du principe que les directions sont modélisées par un cercle de 36 pixels de rayon
+    if(new_dir == 'B' && last_dir->y >= 0 && last_dir->y < 36){
         last_dir->y += 2;
         if(last_dir->x < 0){
             last_dir->x += 2;
@@ -48,7 +51,7 @@ void change_dir(point_t* last_dir, char new_dir){//note, certaines situations ne
 
 
 void snake_move(snake_t *snake){
-    queue_t *elem;
+    node_t *elem;
     const Uint8 *state = SDL_GetKeyboardState(NULL);
     SDL_PumpEvents();
     if (state[SDL_SCANCODE_UP]) {
