f62e51144846e07f1a21a90d06be283364269cb8
ThLegras <legrastheo@gmail.com>
2018-04-30 12:22:57 +0200

    faire un snake

--- src/gameplay/snake/snake.c
+++ src/gameplay/snake/snake.c
@@ -63,17 +63,17 @@ void change_dir(point_t *last_dir, char new_dir) {//note, certaines situations n
             last_dir->x -= 2;
         }
     }
-    if (last_dir->.x < 0){//si on passe à travers l'un des bords de la map on apparait de l'autre cote
-        last_dir->.x += MAX_X;
+    if (last_dir->x < 0){//si on passe à travers l'un des bords de la map on apparait de l'autre cote
+        last_dir->x += MAX_X;
     }
-    if (last_dir->.x > MAX_X){//si on passe à travers l'un des bords de la map on apparait de l'autre cote
-        last_dir->.x -= 0;
+    if (last_dir->x > MAX_X){//si on passe à travers l'un des bords de la map on apparait de l'autre cote
+        last_dir->x -= 0;
     }
-    if (last_dir->.y < 0){//si on passe à travers l'un des bords de la map on apparait de l'autre cote
-        last_dir->.y += MAX_Y;
+    if (last_dir->y < 0){//si on passe à travers l'un des bords de la map on apparait de l'autre cote
+        last_dir->y += MAX_Y;
     }
-    if (last_dir->.y > MAX_Y){//si on passe à travers l'un des bords de la map on apparait de l'autre cote
-        last_dir->.y -= 0;
+    if (last_dir->y > MAX_Y){//si on passe à travers l'un des bords de la map on apparait de l'autre cote
+        last_dir->y -= 0;
     }
 }
 
--- src/main.c
+++ src/main.c
@@ -12,15 +12,18 @@ void update();
 
 
 int main(int argc, char *argv[]) {
-    snake_t snake1, snake2;
-    snake_init(&snake1);
-    snake_init(&snake2);
+    snake_t *snake1, *snake2;
+    snake1 = malloc(sizeof(snake_t));
+    snake2 = malloc(sizeof(snake_t));
+    snake_init(snake1);
+    snake_init(snake2);
     if (!init()){
         return -1;
     }
 
     while (!SDL_HasEvent(SDL_QUIT)) {
-
+        snake_move(snake1);
+        snake_render(snake1);
     }
     close();
     return 0;
