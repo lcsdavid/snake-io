f345ccdf33a6859b0158c1bb3aa23b1f44153716
Lucas David <26874907+lcsdavid@users.noreply.github.com>
2018-05-03 16:45:42 +0200

    main update snake_init

--- src/main.c
+++ src/main.c
@@ -12,18 +12,16 @@ void update();
 
 
 int main(int argc, char *argv[]) {
-    snake_t *snake1, *snake2;
-    snake1 = malloc(sizeof(snake_t));
-    snake2 = malloc(sizeof(snake_t));
-    snake_init(snake1);
-    snake_init(snake2);
+    snake_t snake1, snake2;
+    snake_init(&snake1, 3, NULL);
+    snake_init(&snake2, 2, &(point_t){2,1});
     if (!init()){
         return -1;
     }
 
     while (!SDL_HasEvent(SDL_QUIT)) {
-        snake_move(snake1);
-        snake_render(snake1);
+        snake_move(&snake1);
+        snake_render(&snake1);
     }
     close();
     return 0;
