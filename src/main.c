dfa5c0127b3f74b8467759b9a2ce5ba583014595
lcsdavid <david-lucas-69@hotmail.fr>
2018-05-04 12:25:48 +0200

    remove sdl

--- src/main.c
+++ src/main.c
@@ -3,32 +3,47 @@
 #include <stdint.h>
 
 #include <SDL.h>
-#include "sdl/sdl.h"
 #include "gameplay/snake/snake.h"
 
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+snake_t snake_1, snake_2;
+
+bool init();
 void input();
 void render();
 void update();
+void close();
 
 int main(int argc, char *argv[]) {
-    snake_t snake1, snake2;
-    snake_load_texture();
-    snake_init(&snake1, 3, NULL);
-    snake_init(&snake2, 2, &(point_t){2,1});
-    if (!init()){
-        return -1;
-    }
+    /* snake_load_texture();
+    snake_init(&snake_1, 3, NULL); */
+    // snake_init(&snake2, 2, &(point_t){2,1});
+    if (!init()) return -1;
     while (!SDL_HasEvent(SDL_QUIT)) {
-        snake_move(&snake1);
-        snake_render(&snake1);
-        SDL_RenderPresent(get_renderer());
+        //update();
+        render();
     }
     close();
     return 0;
 }
 
-void render() {
-
+bool init() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL_Init(): %s\n", SDL_GetError());
+        return false;
+    }
+    atexit(SDL_Quit);
+    /*if(IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
+        fprintf(stderr, "IMG_Init(): %s\n", SDL_GetError());
+        return false;
+    }*/
+    SDL_CreateWindowAndRenderer(800, 480, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    return true;
 }
 
 void input() {
@@ -39,4 +54,20 @@ void input() {
         default:
             break;
     }
+}
+
+void render() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    // snake_render(&snake_1);
+    SDL_RenderPresent(renderer);
+}
+
+void update() {
+    snake_move(&snake_1);
+}
+
+void close() {
+    if(window)
+        SDL_DestroyWindow(window);
+    SDL_Quit();
 }
