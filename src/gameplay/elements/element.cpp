84ad268f5f0bf852116f180faa26cad7d3e4a20b
ThLegras <legrastheo@gmail.com>
2018-05-23 15:05:11 +0200

    ajout de fonction a elements

--- src/gameplay/elements/element.cpp
+++ src/gameplay/elements/element.cpp
@@ -3,9 +3,43 @@
 //
 #include "element.h"
 
-typedef struct element{
-    point_t position;
-    int type;
 
 
-};
+void init_bombe(element_t *element, point_t position){
+    element->element_effet = effet_bombe;
+    element->position = position;
+    element->element_render = render_bombe;
+}
+void init_mur(element_t *element, point_t position){
+    element->element_effet = effet_mur;
+    element->position = position;
+    element->element_render = render_mur;
+}
+
+void init_pomme(element_t *element, point_t position){
+    element->element_effet = effet_pomme;
+    element->position = position;
+    element->element_render = render_pomme;
+}
+
+
+void render_bombe(void);
+void render_mur(void);
+void render_pomme(void);
+
+void effet_bombe(void *snake){
+    snake_t *snake_1 = (snake_t *)snake;
+    snake_diminish(snake_1);
+}
+
+void effet_mur(void *snake){
+    snake_t *snake_1 = (snake_t *)snake;
+    while(snake_1->lenght < 0){
+        snake_diminish(snake_1);
+    }
+}
+
+void effet_pomme(void *snake){
+    snake_t *snake_1 = (snake_t *)snake;
+    snake_grow(snake_1);
+}
