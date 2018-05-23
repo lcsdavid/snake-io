7f957434fbea7c5b24ba85d3d355c22ca55ab92f
ThLegras <legrastheo@gmail.com>
2018-05-23 14:19:40 +0200

    ajout d'elements

---
+++ src/gameplay/elements/element.cpp
@@ -0,0 +1,11 @@
+//
+// Created by user on 23/05/2018.
+//
+#include "element.h"
+
+typedef struct element{
+    point_t position;
+    int type;
+
+
+};
---
+++ src/gameplay/elements/element.h
@@ -0,0 +1,15 @@
+//
+// Created by user on 23/05/2018.
+//
+
+#ifndef SNAKE_IO_ELEMENT_H
+#define SNAKE_IO_ELEMENT_H
+
+#include "../../standard/math/point.h"
+
+typedef struct element element_t;
+
+
+
+
+#endif //SNAKE_IO_ELEMENT_H
