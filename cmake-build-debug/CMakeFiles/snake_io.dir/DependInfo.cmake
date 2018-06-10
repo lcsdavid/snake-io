d838973274fbf20110eb0c62252b94b6b836c25e
Lucas David <26874907+lcsdavid@users.noreply.github.com>
2018-06-10 22:42:42 +0200

    Up

---
+++ cmake-build-debug/CMakeFiles/snake_io.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/gameplay/appstate.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/gameplay/appstate.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/gameplay/elements/element.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/gameplay/elements/element.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/gameplay/gamestate.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/gameplay/gamestate.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/gameplay/map/map.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/gameplay/map/map.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/gameplay/menu/button.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/gameplay/menu/button.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/gameplay/menu/menu.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/gameplay/menu/menu.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/gameplay/snake/snake.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/gameplay/snake/snake.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/main.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/main.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/collection/forward_node.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/collection/forward_node.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/collection/iterator.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/collection/iterator.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/collection/list.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/collection/list.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/collection/list_iterator.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/collection/list_iterator.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/collection/node.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/collection/node.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/collection/queue.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/collection/queue.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/collection/stack.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/collection/stack.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/collection/vector.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/collection/vector.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/function/function.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/function/function.c.obj"
+  "D:/Users/lucasdavid/Documents/GitHub/snake-io/src/standard/math/point.c" "D:/Users/lucasdavid/Documents/GitHub/snake-io/cmake-build-debug/CMakeFiles/snake_io.dir/src/standard/math/point.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/SDL2/win32/x86_64-w64-mingw32/include/SDL2"
+  "../lib/SDL2_image/win32/x86_64-w64-mingw32/include/SDL2"
+  "../lib/SDL2_ttf/win32/x86_64-w64-mingw32/include/SDL2"
+  "../lib/SDL2_mixer/win32/x86_64-w64-mingw32/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
